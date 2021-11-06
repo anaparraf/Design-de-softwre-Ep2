086dd08551877d6919d7ae683e589f29820036e7
Ana Beatriz <anabpf@al.insper.edu.br>
2021-11-06 15:05:20 -0300

    mudando todas as funções pra só um arquivo

--- somapeças.py
+++
@@ -1,8 +0,0 @@
-#Soma peças do dominó
-def soma_pecas(pecas_jogador):
-    valor_de_cada = []
-    for peca in pecas_jogador:
-        valor_de_cada.append(peca[0]+peca[1])
-
-    return sum(valor_de_cada)   
-
