66cdf529edd931255cd0a5120a2ab16c87faee5d
Ana Beatriz <anabpf@al.insper.edu.br>
2021-11-06 23:57:47 -0300

    importanto as funçoes

--- inicio.py
+++ inicio.py
@@ -1,7 +1,11 @@
+from funçoes import cria_pecas,  inicia_jogo , verifica_ganhador , posicoes_possiveis , adiciona_na_mesa , soma_pecas 
+
 # Início 
 with open('arquivo_textoinicio.txt', 'r') as arquivo:
     conteudo_completo = arquivo.read()
 print(conteudo_completo)
 
 # Pergunta qtd de jogadores
-numero_jogadores = input('Quantas pessoas jogarão? (2-4)')
+numero_jogadores = int(input('Quantas pessoas jogarão? (2-4)'))
+
+
