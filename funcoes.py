25ec36121593674b8d9d779ff7faf9b1e85363ca
Ana Beatriz <anabpf@al.insper.edu.br>
2021-11-07 21:42:25 -0300

    movendo funçaõ formata peças para arquivo  funções

--- funcoes.py
+++ funcoes.py
@@ -138,3 +138,16 @@ def adiciona_na_mesa(peca, mesa):
         nova_mesa.append (inverter)
         
     return nova_mesa
+
+
+#Formata peças de [a,b] para [a|b]
+
+def formata_peca(lista_pecas):
+    pecas_formatadas = []
+    for peca in lista_pecas:
+        p = str(lista_pecas)
+        nova_peca = p.replace(',',' |')
+        pecas_formatadas.append(nova_peca)
+        
+        return pecas_formatadas
+
